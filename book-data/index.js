fa3330b989ddb102c8451c54b1a68abeb90a6022
JovanRuzic <ruzicjovan96@gmail.com>
2020-04-16 08:47:06 +0200

    Book data inserted

--- book-data/index.js
+++ book-data/index.js
@@ -1,3 +1,29 @@
-let books = ["test"];
+let books = [
+    {
+        id: 0,
+        name: "Harry Potter and the Philosopher's Stone",
+        author: "J.K. Rowling"
+    },
+    {
+        id: 1,
+        name: "The Three Musketeers",
+        author: "Alexandre Dumas"
+    },
+    {
+        id: 2,
+        name: "Don Quixote",
+        author: "Miguel de Cervantes"
+    },
+    {
+        id: 3,
+        name: "The Lord of the Rings",
+        author: "J.R.R. Tolkien"
+    },
+    {
+        id: 4,
+        name: "A Game of Thrones",
+        author: "George R.R. Martin"
+    }
+];
 
 module.exports = books;
